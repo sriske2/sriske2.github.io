ea2af51e2771c4a4c1c2692af440a4fd1d5886d4
Skyler Riske <sriske2@users.noreply.github.com>
2023-07-20 08:56:50 -0700

    add post file generator

---
+++ generate_post.sh
@@ -0,0 +1,25 @@
+# Skyler Riske 2023
+# Generate a template blog post for my website.
+# Usage: "./generate_post.sh example-title"
+
+if [ -z "$1" ]; then
+    echo "no title supplied"
+    exit 1
+fi
+HEADER_DATE=$(date +"%Y-%m-%d %H:%M:%S %z")
+DIR="_posts"
+FILENAME="$(date +%Y-%m-%d)-$1.md"
+HEADER="--- 
+layout: post
+title: \"$1\"
+date: $HEADER_DATE
+tags: [general]
+---"
+echo $FILENAME
+echo $HEADER
+if [ -e $FILENAME ]
+then
+    echo "post exists already"
+else
+    echo "$HEADER" > $DIR/$FILENAME
+fi
