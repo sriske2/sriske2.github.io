b2d91c699391d162e7d7171005fae784eed4811d
Skyler Riske <sriske2@users.noreply.github.com>
2023-07-20 20:08:52 -0700

    fix day calculator off by 1

--- assets/projects/daysSinceCalculator/daysSinceCalculator.js
+++ assets/projects/daysSinceCalculator/daysSinceCalculator.js
@@ -1,38 +1,19 @@
 
-let then = new Date();
-then.setUTCMonth(6);
-then.setUTCDate(26);
-then.setUTCFullYear(2023);
-then.setUTCMinutes(0);
-then.setUTCHours(0);
-then.setUTCSeconds(0);
-const now = new Date();
-let diff = now - then;
-const milliToSec = 1/1000;
-const secToMin = 1/60;
-const MinToHour = 1/60;
-const HourToDay = 1/24;
-let result = diff * milliToSec * secToMin * MinToHour * HourToDay;
-
-let withToday = Math.ceil(result);
-let withoutToday = Math.floor(result);
-console.log(withToday + ", " + withoutToday);
+const milliToSec = 1000;
+const secToMin = 60;
+const MinToHour = 60;
+const HourToDay = 24;
 
 let resultBox = document.getElementById("result");
 let datePicker = document.getElementById("datepicker");
 
 datePicker.addEventListener("change", (e) => {
-    console.log("test", e.target.value);
     let dateStr = e.target.value;
     let then = new Date(dateStr);
     let now = new Date();
-    now.setUTCHours(0);
-    now.setUTCMinutes(0);
-    now.setUTCSeconds(0);
-    
-    let diff = now - then;
-    let result = Math.floor(diff * milliToSec * secToMin * MinToHour * HourToDay);
-    console.log(result);
+    let now_clean = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    let diff = now_clean - then;
+    let result = Math.floor(diff / (milliToSec * secToMin * MinToHour * HourToDay));
     resultBox.textContent = result;
 
 });
